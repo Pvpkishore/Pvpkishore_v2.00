badf355f590bd2a53312506000bb5cdfc2064622
PONNALA VENKATA PADMA KISHORE <139259939+Pvpkishore@users.noreply.github.com>
2025-01-14 19:31:37 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ import linkedin from "../../assets/icons-white/icons8-linkedin-white.png";
 import github from "../../assets/icons-white/github-white-icon.png";
 import TypedAnimation from "../Typed/Typed";
 import { gsap } from "gsap";
-import resume from "../../assets/resume/Pvpkishore_SoftwareDevloper_resume.pdf";
+import resume from "../../assets/resume/Pvpkishore_softwareDevloper_Resume.pdf";
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import GlitchText from "../Effects/Glitch";
 import RippleButton from "../Effects/RippleEffect";
